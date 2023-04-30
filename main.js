a715dea72beb31527a962207980d0b4291350da8
Esteban <pachondanielesteban@gmail.com>
2023-04-30 15:35:06 -0500

    add responsive design. Final version

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const closeMenuIcon = document.querySelector('.logo-close');
 
 burgerIcon.addEventListener('click', openMenuMobile);
 closeMenuIcon.addEventListener('click', closeMenuMobile);
+window.addEventListener('load',responsiveNavbar);
+window.addEventListener('resize',responsiveNavbar);
 
+function responsiveNavbar() {
+    if (window.innerWidth>=890) {
+        navBar.classList.remove('inactive');
+    } else {
+        navBar.classList.add('inactive')
+    }
+}
 function openMenuMobile() {
     navBar.classList.toggle('inactive');
 }
